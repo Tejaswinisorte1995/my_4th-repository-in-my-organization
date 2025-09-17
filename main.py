a994c1d62c1f8f357456e8a430d35a55e01a6a79
Tejaswinisorte1995 <sorte.tejaswini@gmail.com>
2025-09-17 12:05:33 +0530

    this is my 4th commit regarding matching bug for master cluster concept

---
+++ main.py
@@ -0,0 +1,11 @@
+num1 = 32
+num2 = 39
+num3 = 45
+if num1>num2 and num1>num3:
+    print(num1,"is greater than", num2, "&", num3)
+elif num2>num1 and num2>num3:
+    print(num2,"is greater than", num1, "&", num3)
+elif num3>num1 and num3>num2:
+    print(num3,"is greater than", num1, "&", num2)
+
+
